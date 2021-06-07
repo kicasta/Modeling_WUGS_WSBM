5bd00e7de20796cd3b79cdef41c8d2deb39c8823
kicasta <kicasta@gmail.com>
2021-06-07 16:48:51 +0200

    added missing function

--- src/draw_wugs.py
+++ src/draw_wugs.py
@@ -1,10 +1,22 @@
 import plot_utils as pltutil
 import wsbm as wsbm
 import pickle
-
+from docopt import docopt
 
 
 def main():
+
+  # Get the arguments
+  args = docopt("""Generates a plot with the graph partition showing a comparison between observed and inferred values
+  Usage:
+  draw_wugs.py <path_to_graphs> <path_to_pickle> <path_to_save>
+      
+  Arguments:w
+
+  <path_to_graphs> = path to the directory where all the graphs are stored  
+  <path_to_pickle> = path to the pickled dictionary containing the best fit for each graph
+  <path_to_save> = path to where the images should be stored   
+  """)
 
   path= args['<path_to_graphs>']
   output_path = args['<path_to_pickle>']
